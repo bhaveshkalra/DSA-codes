4c7bbd672e5fb9f438780910982fb8ccf8a8e549
Aayush Jain <aj.rt.9597@gmail.com>
2020-10-01 18:21:15 +0530

    Update bubble_sorting.c
    Added print function to the algorithm

--- Sorting/bubble_sorting.c
+++ Sorting/bubble_sorting.c
@@ -5,15 +5,16 @@ void main()
 	printf("input the number of elements of the list\n ");
 	scanf("%d",&n);
 	printf("\n Input the elements of the list\n");
-	for(i=0;i<n;i++)
-	{scanf("%d",&list[i]);}
+	for(i=0;i<n;i++){
+		scanf("%d",&list[i]);
+	}
 	printf("List Input :\n");
 	for(i=0;i<n;i++)	
 	{	printf("%d\n",list[i]);
 	}
-	bubble_sort(n,list);
+	applyBubbleSort(n,list);
 }
-void bubble_sort(int n, int arr[])
+void applyBubbleSort(int n, int arr[])
 {	int temp,i,j;
 	for(i=0;i<n;i++)
 	{	for(j=0;j<n-i-1;j++)
@@ -26,8 +27,12 @@ void bubble_sort(int n, int arr[])
 			}
 		}
 	}
-	printf("\n Sorted list is as follows:\n");
-	for(i=0;i<n;i++)
+ 	printArray(n, arr);
+} 
+
+void printArray(int size, int arr[]) {
+	printf("\n Sorted list\n");
+	for(i=0;i<size;i++)
 	{	printf("%d\n",arr[i]);
 	}
-} 
+}
